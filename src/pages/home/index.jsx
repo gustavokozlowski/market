baf79da6d02e237763ec4c1b380f4a41b047cb23
GustavoKozlowski <gustavokozlowski96@gmail.com>
2023-07-30 21:47:02 -0300

    feat: produtos da FakeAi addicionados com sucesso

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,25 @@
-import { Product } from "../../components/product"
-import { ProductList } from "../../components/product-list"
-import { Slider } from "../../components/slider"
-
+import { api } from "../../services/api/api";
+import { ProductList } from "../../components/product-list";
+import { Slider } from "../../components/slider";
+import { useState, useEffect } from "react";
 
 export const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    api.get("/products/?limit=4")
+      .then((response) => {
+        console.log(response);
+        setProducts(response.data);
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro : " + err);
+      });
+  }, []);
+
   return (
     <>
-        <Slider />
-        <ProductList products=""  />
+      <Slider />
+      <ProductList products={products} />
     </>
-  )
-}
+  );
+};
